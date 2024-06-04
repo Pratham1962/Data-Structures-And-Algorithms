5462827d31ad2f88d43f3396032c63a2f3c472a2
Pratham1962 <148983275+Pratham1962@users.noreply.github.com>
2024-06-04 17:03:14 +0530

    Solution added

---
+++ Bit_Manipulation/Power_Of_Two/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        if(n<0) return false;
+        
+        int cnt=0;
+
+        for(int i=0;i<32;i++){
+
+            if((n & (1<<i))!=0) cnt++;
+        }
+
+        return cnt==1;
+    }
+}
+
