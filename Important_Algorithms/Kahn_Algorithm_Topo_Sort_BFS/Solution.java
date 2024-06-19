6693214bea695d88a239120fe94fac72a74f6abb
Pratham1962 <148983275+Pratham1962@users.noreply.github.com>
2024-06-19 14:31:48 +0530

    Update Solution.java

--- Important_Algorithms/Kahn_Algorithm_Topo_Sort_BFS/Solution.java
+++ Important_Algorithms/Kahn_Algorithm_Topo_Sort_BFS/Solution.java
@@ -33,3 +33,28 @@ public class Main {
             }
         }
         
+        return ans;
+    }
+    
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        adj.get(2).add(3);
+        adj.get(3).add(1);
+        adj.get(4).add(0);
+        adj.get(4).add(1);
+        adj.get(5).add(0);
+        adj.get(5).add(2);
+        
+        int[] topoSort = kahnAlgorithm(V, adj);
+        
+        for (int it : topoSort) {
+            System.out.print(it + " ");
+        }
+    }
+}
